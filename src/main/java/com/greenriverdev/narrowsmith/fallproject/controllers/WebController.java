04e50e8ddffdd9343ad892ef31238b3c3f0054f6
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-09-29 12:36:10 -0700

    created static home page, added CSS to styles for home page, tested webController, set port to 8081

--- src/main/java/com/greenriverdev/narrowsmith/fallproject/controllers/WebController.java
+++ src/main/java/com/greenriverdev/narrowsmith/fallproject/controllers/WebController.java
@@ -2,6 +2,7 @@ package com.greenriverdev.narrowsmith.fallproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class WebController
@@ -11,11 +12,11 @@ public class WebController
 
     //the methods below will define a route to a page
 
-    //http://localhost:8080/home
+    //http://localhost:8081/home
     @GetMapping("home")
     public String getHomepage()
     {
         //the name of the view (in the templates directory)
-        return "home_page";
+        return "home";
     }
 }
