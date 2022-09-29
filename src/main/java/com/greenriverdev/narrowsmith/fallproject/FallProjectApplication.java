12120e1c0683caa72704d4d1ce0014656b9e80ae
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-09-28 19:37:22 -0700

    Test push modified file

--- src/main/java/com/greenriverdev/narrowsmith/fallproject/FallProjectApplication.java
+++ src/main/java/com/greenriverdev/narrowsmith/fallproject/FallProjectApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FallProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FallProjectApplication.class, args);
+
+        System.out.println("Hello World");
     }
 
 }
