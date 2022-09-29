821687c5752037b91a053f42207446ba6a67ef25
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-09-28 19:30:02 -0700

    Initial commit

---
+++ src/main/java/com/greenriverdev/narrowsmith/fallproject/FallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.greenriverdev.narrowsmith.fallproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenriverdev/narrowsmith/fallproject/FallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenriverdev.narrowsmith.fallproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
