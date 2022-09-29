cf7ae85359193e594248cdcd7697f2e779098559
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-09-29 16:55:03 -0700

    added javadoc to controller class, cleaned home page

--- src/main/java/com/greenriverdev/narrowsmith/fallproject/controllers/WebController.java
+++ src/main/java/com/greenriverdev/narrowsmith/fallproject/controllers/WebController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+/**
+ * A Controller class that will define routes to future pages
+ * We will also connect with Services here
+ */
 @Controller
 public class WebController
 {
