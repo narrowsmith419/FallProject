5997ee7dcde3f6782ab82a004e33ddcd0147e98b
narrowsmith419 <arrowsmith.nathan@gmail.com>
2022-10-16 12:14:52 -0700

    Added javaDocs to TrailService class

--- src/main/java/com/greenriverdev/narrowsmith/fallproject/services/TrailService.java
+++ src/main/java/com/greenriverdev/narrowsmith/fallproject/services/TrailService.java
@@ -4,21 +4,39 @@ import com.greenriverdev.narrowsmith.fallproject.models.Trail;
 import com.greenriverdev.narrowsmith.fallproject.models.TrailDifficulty;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * service layer to accompany WebApiTrail controller
+ * locally stores Trail objects, provides CRUD access to these Trail objects
+ * @author Nathan Arrowsmith
+ * @version 1.0
+ */
 @Service
 public class TrailService
 {
     private List<Trail> trails = new ArrayList<>(List.of(
-            new Trail(813, 483, "WA", "Duthie Hill", "Paramount", false, TrailDifficulty.DOUBLE_BLACK),
-            new Trail(758, 465, "WA", "Duthie Hill", "Semper Dirticus", false, TrailDifficulty.DOUBLE_BLACK),
-            new Trail(1007, 481, "WA", "Duthie Hill", "Voodoo Child", false, TrailDifficulty.BLUE)
+            new Trail(813, 483, "WA", "Duthie Hill", "Paramount",
+                    false, TrailDifficulty.DOUBLE_BLACK),
+            new Trail(758, 465, "WA", "Duthie Hill", "Semper Dirticus",
+                    false, TrailDifficulty.DOUBLE_BLACK),
+            new Trail(1007, 481, "WA", "Duthie Hill", "Voodoo Child",
+                    false, TrailDifficulty.BLUE)
     ));
 
+    /**
+     * @param length length of trail
+     * @param elevation elevation of the trail
+     * @param state state where trail is located
+     * @param trailSystem trail system where trail is located
+     * @param name name of the trail
+     * @param multiDirectional is it a one way or two-way trail?
+     * @param difficulty { GREEN, BLUE, BLACK, DOUBLE_BLACK, PRO }
+     * @return newly created Trail object
+     */
     //CREATE
     public Trail addTrail(int length, int elevation, String state, String trailSystem,
                           String name, boolean multiDirectional, TrailDifficulty difficulty)
@@ -30,6 +48,9 @@ public class TrailService
     }
 
 
+    /**
+     * @return all Trail objects
+     */
     //READ
     public List<Trail> allTrails()
     {
@@ -37,6 +58,17 @@ public class TrailService
     }
 
 
+    /**
+     * @param id unique Identifier for Trail object
+     * @param length length of trail
+     * @param elevation elevation of the trail
+     * @param state state where trail is located
+     * @param trailSystem trail system where trail is located
+     * @param name name of the trail
+     * @param multiDirectional is it a one way or two-way trail?
+     * @param difficulty { GREEN, BLUE, BLACK, DOUBLE_BLACK, PRO }
+     * @return newly altered Trail object
+     */
     //UPDATE
     public Trail updateTrail(UUID id, int length, int elevation, String state, String trailSystem,
                              String name, boolean multiDirectional, TrailDifficulty difficulty)
@@ -68,6 +100,9 @@ public class TrailService
     }
 
 
+    /**
+     * @param id of Trail object to be deleted
+     */
     //DELETE
     public void deleteTrail(UUID id)
     {
@@ -77,6 +112,10 @@ public class TrailService
     }
 
 
+    /**
+     * @param queryValue trail name of Trail to search
+     * @return all Trail objects with matching trail name
+     */
     //READ
     public List<Trail> searchTrails(String queryValue)
     {
@@ -87,6 +126,10 @@ public class TrailService
     }
 
 
+    /**
+     * @param id Unique identifier of Trial object
+     * @return whether Trail object exists with searching ID
+     */
     public boolean trailExists(UUID id)
     {
         //add all trails with matching id to list, check if list is empty
@@ -96,4 +139,10 @@ public class TrailService
     }
 
 
+    @Override
+    public String toString() {
+        return "TrailService{" +
+                "trails=" + trails +
+                '}';
+    }
 }
